677ae55b0f8a4aad9c8b7d6b9dca3612aacaf236
Sandeep Chenna Kumar <sandeep.chenna@ibm.com>
2026-08-21 09:27:03 +0530

    ran tf fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 resource "random_id" "suffix" {
-    byte_length = 4
+  byte_length = 4
 }
 
 resource "aws_s3_bucket" "test" {
-    bucket = "tf-lab-test2-bucket-${random_id.suffix.hex}"
+  bucket = "tf-lab-test2-bucket-${random_id.suffix.hex}"
 }

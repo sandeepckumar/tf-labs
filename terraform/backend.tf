8fb7de82c3db890dcb9790504abb06ca683d579a
Sandeep Chenna Kumar <sandeep.chenna@ibm.com>
2026-09-03 15:30:44 +0530

    iam clean start

--- terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "tf-lab-state-bucket-fri210826"
-    key          = "lab/terraform.tfstate"
-    region       = "us-east-1"
-    encrypt      = true
-    use_lockfile = true
-  }
-}
--- terraform/main.tf
+++
@@ -1,7 +0,0 @@
-resource "random_id" "suffix" {
-  byte_length = 4
-}
-
-resource "aws_s3_bucket" "test" {
-  bucket = "tf-lab-test2-bucket-${random_id.suffix.hex}"
-}
